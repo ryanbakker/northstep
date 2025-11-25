43c88f1e4eccf6a23ab8b4dd857014a6e7ac531b
Ryan <ryanbakker@outlook.co.nz>
2025-11-25 17:45:04 +1300

    Add auth & schemas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,35 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { ModalProvider } from "@/contexts/ModalContext";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Slot } from "expo-router";
+import { useColorScheme } from "react-native";
+import { TamaguiProvider } from "tamagui";
+import "../tamagui-web.css";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { tamaguiConfig } from "../tamagui.config";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ClerkProvider tokenCache={tokenCache}>
+        <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+          <ModalProvider>
+            <ThemeProvider
+              value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+            >
+              <Slot />
+            </ThemeProvider>
+          </ModalProvider>
+        </TamaguiProvider>
+      </ClerkProvider>
+    </SafeAreaProvider>
   );
 }
